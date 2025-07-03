d50b4ea0c01f5abd53534d1ff7e241186fab6180
shashiThorat <shashi_thorat@rediffmail.com>
2025-07-03 22:39:48 +0530

    Update Box.java

--- src/main/java/com/pack/combopack/bean/Box.java
+++ src/main/java/com/pack/combopack/bean/Box.java
@@ -15,7 +15,25 @@ public class Box<T extends Packable> {
 		this.id = id;
 	}
 
-	
+	@Override
+	public String toString() {
+		return "Box [id=" + id + ", items=" + items + "]";
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public double getMaxWeight() {
+		return maxWeight;
+	}
+
+
+
+	public void setItems(List<T> itemList) {
+		this.items = itemList;
+	}
+
 
 	
 	
